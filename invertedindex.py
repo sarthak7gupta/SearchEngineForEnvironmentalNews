dd7ba83d8c614cda827ffe0a8fab763cae82620d
Raunak Sengupta <42582079+raunaks42@users.noreply.github.com>
2020-11-12 09:46:07 +0530

    Library and own implementation test

--- invertedindex.py
+++ invertedindex.py
@@ -1,87 +1,86 @@
-import math 
+import math
+import time
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+'''
 def union2(dict1, dict2):
     return dict(list(dict1.items()) + list(dict2.items()))
+'''
+def union2(list1, list2):
+	return list1 + list2
 
-class Node:
-	def __init__(self,docID):
-		self.docID = docID
-		self.tf_idf = None
-		self.tf = 1
-
-class PostingList:
-	list_dict = dict()
-	
+class DocEntry:
 	def __init__(self):
-		self.list_dict = dict()
-	
-	def addEntry(self,docID):
-		if docID not in self.list_dict:
-			self.list_dict[docID] = Node(docID)
-			return None
-		else:
-			self.list_dict[docID].tf += 1
-			return "INCREASE"
-	
+		self.tf_idf = None
+		self.tf = 0
+
 class TermEntry:
 	def __init__(self):
-		self.docFreq  = 0
-		self.postingList = PostingList()
+		self.idf = None
+		self.docFreq = 0
+		self.postingList = dict()
 
-	def addDoc(self,docID):
-		if self.postingList.addEntry(docID) == None:
+	def addDoc(self, docID):
+		if docID not in self.postingList:
+			self.postingList[docID] = DocEntry()
 			self.docFreq += 1
+		self.postingList[docID].tf += 1
 
 class InvertedIndex:
-	def __init__(self,numDocs):
+	def __init__(self):
 		self.__indexDictionary = dict()
-		self.numDocs = numDocs
 
 	def __addTerm(self,term):
 		self.__indexDictionary[term] = TermEntry()
 
-	def addEntry(self,term,docID):
+	def addEntry(self, term, docID):
 		if term not in self.__indexDictionary:
 			self.__addTerm(term)
 		self.__indexDictionary[term].addDoc(docID)
 
-	def getPostingList(self,term):
-		if term not in self.__indexDictionary:
-			return None 
-		return self.__indexDictionary[term].postingList.list_dict
-	
 	def getTerms(self):
 		return list(self.__indexDictionary.keys())
 
-	def getTF(self,term,docID):
-		try:
-			return self.__indexDictionary[term].postingList.list_dict[docID].tf
-		except:
-			return None
+	def getPostingList(self, term):
+		if term in self.__indexDictionary:
+			return list(self.__indexDictionary[term].postingList.keys())
+		return None
 
-	def getDocFreq(self,term):
-		try:
+	def getDocFreq(self, term):
+		if term in self.__indexDictionary:
 			return self.__indexDictionary[term].docFreq
-		except:
-			return None
-	def getIDF(self,term):
-		try:
-			return math.log2(self.numDocs/self.__indexDictionary[term].docFreq)
-		except:
-			return None
+		return None
 
-	def calTFIDF(self,term,docID):
-		if term not in self.__indexDictionary:
-			return None 
-		pl = self.__indexDictionary[term].postingList.list_dict
-		if docID not in pl:
-			return None 
-		docFreq = self.__indexDictionary[term].docFreq
-		tf = pl[docID].tf
-		idf = math.log2(self.numDocs / docFreq )
-		pl[docID].tf_idf = tf * idf
-		return pl[docID].tf_idf
+	def getIDF(self, term):
+		if term in self.__indexDictionary:
+			return self.__indexDictionary[term].idf
+		return None
 
+	def getTF(self, term, docID):
+		if term in self.__indexDictionary and docID in self.__indexDictionary[term].postingList:
+				return self.__indexDictionary[term].postingList[docID].tf
+		return None
+
+	def getTFIDF(self,term,docID):
+		if term in self.__indexDictionary and docID in self.__indexDictionary[term].postingList:
+				return self.__indexDictionary[term].postingList[docID].tf_idf
+		return None
+
+	def calcIDF(self, term, numDocs):
+		if term in self.__indexDictionary:
+			self.__indexDictionary[term].idf = math.log(numDocs/self.getDocFreq(term)) + 1
+
+	def calcTFIDF(self, term, docID):
+		if term in self.__indexDictionary and docID in self.__indexDictionary[term].postingList:
+				tf = math.log(self.getTF(term, docID)) + 1
+				self.__indexDictionary[term].postingList[docID].tf_idf = tf * self.getIDF(term)
+
+	def populateTFIDF(self, numDocs):
+		for term in self.__indexDictionary:
+			self.calcIDF(term, numDocs)
+			for docID in self.__indexDictionary[term].postingList:
+				self.calcTFIDF(term, docID)
+'''
 	def setTFIDF(self,term,docID,tfidf):
 		if term not in self.__indexDictionary:
 			return None 
@@ -90,22 +89,23 @@ class InvertedIndex:
 			return None 
 		pl[docID].tf_idf = tfidf
 		return tfidf
+'''
 
-	def getTFIDF(self,term,docID):
-		if term not in self.__indexDictionary:
-			return None 
-		pl = self.__indexDictionary[term].postingList.list_dict
-		if docID not in pl:
-			return None 
-		return pl[docID].tf_idf
-'''		
 corpus = [
 	"brutus killed caesar brutus",
 	"caesar calpurnia",
 	"brutus friend john"
 ]
 
-index = InvertedIndex(3)
+# LIBRARY TEST (comment lines 26&27)
+
+start_time=time.time()
+
+vectorizer = TfidfVectorizer(smooth_idf=False, sublinear_tf=True)
+X = vectorizer.fit_transform(corpus)
+print(vectorizer.idf_)
+
+index = InvertedIndex()
 index.addEntry('brutus',1)
 index.addEntry('brutus',1)
 index.addEntry('killed',1)
@@ -116,30 +116,37 @@ index.addEntry('brutus',3)
 index.addEntry('friend',3)
 index.addEntry('john',3)
 
+print(time.time()-start_time)
 
-#QUERY = "brutus caesar"
-#Note : not actual doing the cosine simmilarity
 
-#Union of posting lists of "brutus" and "caeasar"
+# OWN TEST
 
-posting_list_brutus = index.getPostingList("brutus")
-posting_list_caesar = index.getPostingList("caesar")
+start_time=time.time()
 
-#print(posting_list_brutus)
-#print(posting_list_caesar)
+index = InvertedIndex()
+index.addEntry('brutus',1)
+index.addEntry('brutus',1)
+index.addEntry('killed',1)
+index.addEntry('caesar',1)
+index.addEntry('caesar',2)
+index.addEntry('calpurnia',2)
+index.addEntry('brutus',3)
+index.addEntry('friend',3)
+index.addEntry('john',3)
 
-posting_list_union = union2(posting_list_brutus,posting_list_caesar)
-#print(posting_list_union) #  3 documetns in union - 1,2,3
+index.populateTFIDF(3)
 
-#Calcluating tf-idf scores
-index.calTFIDF('brutus',1)
-index.calTFIDF('brutus',2)
-index.calTFIDF('brutus',3)
+print(index.getIDF('brutus'))
+print(index.getIDF('caesar'))
+print(index.getIDF('calpurnia'))
+print(index.getIDF('friend'))
+print(index.getIDF('john'))
+print(index.getIDF('killed'))
 
-index.calTFIDF('caesar',1)
-index.calTFIDF('caesar',2)
-index.calTFIDF('caesar',3)
+print(time.time()-start_time)
 
+
+'''
 #Printing result
 print('USING INDEX: ')
 #print(index.getTerms())
@@ -147,6 +154,4 @@ print("term      doc1                  doc2                  doc3")
 print('{}    {}    {}    {}'.format('brutus',index.getTFIDF('brutus',1),index.getTFIDF('brutus',2),index.getTFIDF('brutus',3)))
 print('{}    {}    {}    {}'.format('caesar',index.getTFIDF('caesar',1),index.getTFIDF('caesar',2),index.getTFIDF('caesar',3)))
 print("\n")
-
-
 '''
